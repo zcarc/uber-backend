c0146479d66e447d87442b1542f479bd427616db
wrkbr <zcarc1@gmail.com>
2020-04-17 21:59:03 +0900

    #1.7 API and Schema Structue part Two

--- src/schema.ts
+++ src/schema.ts
@@ -1,4 +1,20 @@
-import { GraphQLServer } from "graphql-yoga";
 import { makeExecutableSchema } from "graphql-tools";
 import { fileLoader, mergeResolvers, mergeTypes } from "merge-graphql-schemas";
 import path from "path";
+
+const allTypes: any = fileLoader(path.join(__dirname, "./api/**/*.graphql"));
+
+// 나중에 배포용으로 빌드를하면 ts에서 js로 바뀌므로 '*'로 설정
+const allResolvers: any = fileLoader(
+  path.join(__dirname, "./api/**/*.resolvers.*")
+);
+
+const mergedTypes: any = mergeTypes(allTypes);
+const mergedResolvers: any = mergeResolvers(allResolvers);
+
+const schema = makeExecutableSchema({
+  typeDefs: mergedTypes,
+  resolvers: mergedResolvers,
+});
+
+export default schema;
