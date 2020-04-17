32f2440a81505335bebe103f9c60d8881606eede
wrkbr <zcarc1@gmail.com>
2020-04-18 05:11:13 +0900

    #1.11 Creating a Virtual Environment on NodeJS

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import { Options } from "graphql-yoga";
-import app from "./app";
 import { createConnection } from "typeorm";
+import app from "./app";
 import connectionOptions from "./ormConfig";
 
 const PORT: number | string = process.env.PORT || 4000;
@@ -15,6 +18,8 @@ const appOptions: Options = {
 
 const handleAppStart = () => console.log(`Listening on port ${PORT}`);
 
-createConnection(connectionOptions).then(() => {
-  app.start(appOptions, handleAppStart);
-});
+createConnection(connectionOptions)
+  .then(() => {
+    app.start(appOptions, handleAppStart);
+  })
+  .catch((error) => console.log(error));
--- src/ormConfig.ts
+++ src/ormConfig.ts
@@ -6,10 +6,10 @@ const connectionOptions: ConnectionOptions = {
   synchronize: true,
   logging: true,
   entities: ["entities/*.*"],
-  host: process.env.DB_ENDPOINT || "localhost",
+  host: process.env.DB_ENDPOINT,
   port: 5432,
-  username: process.env.DB_USERNAME || "postgres",
-  password: process.env.DB_PASSOWRD || "",
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSOWRD,
 };
 
 export default connectionOptions;
