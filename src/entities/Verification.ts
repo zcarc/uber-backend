fc0e59afe4c94e1afc09dde9319067230d12f769
wrkbr <zcarc1@gmail.com>
2020-04-18 19:12:31 +0900

    #1.17 Verification Entity part One

---
+++ src/entities/Verification.ts
@@ -0,0 +1,31 @@
+import {
+  Entity,
+  BaseEntity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from "typeorm";
+
+@Entity()
+class Verification extends BaseEntity {
+  @PrimaryGeneratedColumn() id: number;
+
+  @Column({ type: "text" })
+  target: string;
+
+  @Column({ type: "text" })
+  payload: string;
+
+  @Column({ type: "text" })
+  key: string;
+
+  @Column({ type: "boolean", default: false })
+  used: boolean;
+
+  @CreateDateColumn() createdAt: string;
+
+  @UpdateDateColumn() updatedAt: string;
+}
+
+export default Verification;
