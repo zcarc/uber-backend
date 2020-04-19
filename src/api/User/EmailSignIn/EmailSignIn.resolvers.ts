9a34d3e64255863ecb1e5f380c3632137daa1d1f
wrkbr <zcarc1@gmail.com>
2020-04-19 19:10:23 +0900

    #1.32 EmailSignIn Resolver part One

---
+++ src/api/User/EmailSignIn/EmailSignIn.resolvers.ts
@@ -0,0 +1,35 @@
+import {
+  EmailSignInMutationArgs,
+  EmailSignInResponse,
+} from "../../../types/graph";
+import { Resolvers } from "src/types/resolvers";
+import User from "src/entities/User";
+
+const resolvers: Resolvers = {
+  Mutation: {
+    EmailSignIn: async (
+      _,
+      args: EmailSignInMutationArgs
+    ): Promise<EmailSignInResponse> => {
+      const { email } = args;
+      try {
+        const user = await User.findOne({ email });
+        if (!user) {
+          return {
+            ok: false,
+            error: "No User found with that email",
+            token: null,
+          };
+        }
+      } catch (error) {
+        return {
+          ok: false,
+          error: error.message,
+          token: null,
+        };
+      }
+    },
+  },
+};
+
+export default resolvers;
