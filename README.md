99566b1e5d5188ea7004c26eb0381ca98a35436f
wrkbr <zcarc1@gmail.com>
2020-04-20 09:41:12 +0900

    #.1.38 CompletePhoneVerification part One

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Server for the Uber Clone. GraphQL, Typescript, NodeJS
 ### Public Resolvers:
 - [x] Sign In / Sign Up with Facebook
 - [x] Sign In with Email
-- [ ] Start Phone Number Verification
+- [x] Start Phone Number Verification
 - [ ] Complete Phone Number Verification
 - [ ] Sign Up with Email
 
--- src/entities/Verification.ts
+++ src/entities/Verification.ts
@@ -25,16 +25,17 @@ class Verification extends BaseEntity {
   @Column({ type: "text" })
   key: string;
 
+  @Column({ type: "boolean", default: false })
+  verified: boolean;
+
   @CreateDateColumn() createdAt: string;
 
   @UpdateDateColumn() updatedAt: string;
 
   @BeforeInsert()
   createKey(): void {
-
     if (this.target === PHONE) {
       this.key = Math.floor(Math.random() * 100000).toString();
-      
     } else if (this.target === EMAIL) {
       this.key = Math.random().toString(36).substr(2);
     }
