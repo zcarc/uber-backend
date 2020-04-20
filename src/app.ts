032af64f4e8331f911042e10d8cd62867a2d7d40
wrkbr <zcarc1@gmail.com>
2020-04-20 18:49:44 +0900

    #1.45 Custom Auth Middleware on Express part Two

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import { GraphQLServer } from "graphql-yoga";
 import helmet from 'helmet';
 import logger from 'morgan';
 import schema from './schema';
+import decodeJWT from './utils/decodeJWT';
 
 class App {
   public app: GraphQLServer;
@@ -18,12 +19,15 @@ class App {
     this.app.express.use(cros());
     this.app.express.use(logger("dev"));
     this.app.express.use(helmet());
+    this.app.express.use(this.jwt);
   };
 
   // X-JWT: 원하는대로 지어도 상관없음 (백엔드, 프론트엔드 둘 다 생성해야함)
   private jwt = async(req, res, next): Promise<void> => {
     const token = req.get("X-JWT");
     if(token) {
+      const user = await decodeJWT(token);
+      console.log('user: ', user);
     }
     next();
   }
