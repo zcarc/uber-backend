cd7ab6cf1fb5adacdc301b0abfa47d76c3f1caba
wrkbr <zcarc1@gmail.com>
2020-04-21 17:25:50 +0900

    #1.51 Sending Confirmation Email part Three

--- src/api/User/EmailSignUp/EmailSignUp.resolvers.ts
+++ src/api/User/EmailSignUp/EmailSignUp.resolvers.ts
@@ -2,21 +2,23 @@ import User from "../../../entities/User";
 import Verification from "../../../entities/Verification";
 import { Resolvers } from "../../../types/resolvers";
 import createJWT from "../../../utils/createJWT";
+import { sendVerificationEmail } from "../../../utils/sendEmail";
 import {
-  EmailSignInMutationArgs,
+  EmailSignUpMutationArgs,
   EmailSignUpResponse,
 } from "./../../../types/graph.d";
-
 
 const resolvers: Resolvers = {
   Mutation: {
     EmailSignUp: async (
       _,
-      args: EmailSignInMutationArgs
+      args: EmailSignUpMutationArgs
     ): Promise<EmailSignUpResponse> => {
       const { email } = args;
+
       try {
         const existingUser = await User.findOne({ email });
+
         if (existingUser) {
           return {
             ok: false,
@@ -24,21 +26,40 @@ const resolvers: Resolvers = {
             token: null,
           };
         } else {
-          const newUser = await User.create({ ...args }).save();
+          const phoneVerification = await Verification.findOne({
+            payload: args.phoneNumber,
+            verified: true,
+          });
 
-          if(newUser.email) {
-            const emailVerification = await Verification.create({
-              payload: newUser.email,
-              target: "EMAIL"
-            });
+          if (phoneVerification) {
+            const newUser = await User.create({ ...args }).save();
+
+            if (newUser.email) {
+              const emailVerification = await Verification.create({
+                payload: newUser.email,
+                target: "EMAIL",
+              });
+
+              await sendVerificationEmail(
+                newUser.fullName,
+                emailVerification.key
+              );
+            }
+
+            const token = createJWT(newUser.id);
+
+            return {
+              ok: true,
+              error: null,
+              token,
+            };
+          } else {
+            return {
+              ok: false,
+              error: "You haven't verified your phone number",
+              token: null,
+            };
           }
-
-          const token = createJWT(newUser.id);
-          return {
-            ok: true,
-            error: null,
-            token
-          };
         }
       } catch (error) {
         return {
