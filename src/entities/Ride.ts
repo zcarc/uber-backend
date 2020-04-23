67e4dd0444e8060b3a6e85a4a671886f511ff985
wrkbr <zcarc1@gmail.com>
2020-04-23 13:42:17 +0900

    #1.78 GetRide Resolver

--- src/entities/Ride.ts
+++ src/entities/Ride.ts
@@ -4,9 +4,9 @@ import {
   Column,
   CreateDateColumn,
   Entity,
+  ManyToOne,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
-  ManyToOne,
 } from "typeorm";
 import User from "./User";
 
@@ -17,7 +17,7 @@ class Ride extends BaseEntity {
   @Column({
     type: "text",
     enum: ["ACCEPTED", "FINISHED", "CANCELED", "REQUESTING", "ONROUTE"],
-    default: "ACCEPTED",
+    default: "REQUESTING",
   })
   status: rideStatus;
 
@@ -48,8 +48,14 @@ class Ride extends BaseEntity {
   @Column({ type: "text" })
   duration: string;
 
+  @Column({ nullable: true })
+  passengerId: number;
+
   @ManyToOne((type) => User, (user) => user.ridesAsPassenger)
   passenger: User;
+
+  @Column({ nullable: true })
+  driverId: number;
 
   @ManyToOne((type) => User, (user) => user.ridesAsDriver, { nullable: true })
   driver: User;
