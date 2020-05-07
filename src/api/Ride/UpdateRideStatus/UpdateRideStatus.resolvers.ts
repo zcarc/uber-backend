80d7e03bc01db169cf99115a304077546016425d
wrkbr <zcarc1@gmail.com>
2020-05-07 22:31:06 +0900

    #2.75 Ride Status Subscription

--- src/api/Ride/UpdateRideStatus/UpdateRideStatus.resolvers.ts
+++ src/api/Ride/UpdateRideStatus/UpdateRideStatus.resolvers.ts
@@ -26,7 +26,7 @@ const resolvers: Resolvers = {
                   id: args.rideId,
                   status: "REQUESTING",
                 },
-                { relations: ["passenger"] }
+                { relations: ["passenger", "driver"] }
               );
               if (ride) {
                 ride.driver = user;
@@ -40,10 +40,13 @@ const resolvers: Resolvers = {
                 ride.save();
               }
             } else {
-              ride = await Ride.findOne({
-                id: args.rideId,
-                driver: user,
-              });
+              ride = await Ride.findOne(
+                {
+                  id: args.rideId,
+                  driver: user,
+                },
+                { relations: ["passenger", "driver"] }
+              );
             }
             if (ride) {
               ride.status = args.status;
